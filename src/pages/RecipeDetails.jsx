9f3163bd0f8f672a983dd7f1fce3c44cf4a90c26
Ignacio Lopez <ignaciolopestela12@gmail.com>
2025-01-11 11:34:53 -0300

    add more recipe details info

--- src/pages/RecipeDetails.jsx
+++ src/pages/RecipeDetails.jsx
@@ -6,6 +6,14 @@ import backIcon from '../assets/Expand_left.svg';
 import { fetchData } from '../http';
 import Button from "../components/Button";
 
+const InfoLabel = ({ children }) => {
+    return(
+        <span className="flex flex-row justify-center items-center bg-[#394150] text-[#E5E7EB] rounded-[53px] px-6 py-2">
+            { children }
+        </span>
+    );
+};
+
 const RecipeDetails = () => {
     const [ recipe, setRecipe ] = useState({});
     const { recipeId } = useParams();
@@ -27,9 +35,21 @@ const RecipeDetails = () => {
         navigate('/');
     };
 
+    const ingredients = [];
+
+    if(recipe.length !== 0) {
+        for (let i = 1; i <= 20; i++) {
+            const ingredient = recipe[`strIngredient${ i }`];
+            const measure = recipe[`strMeasure${ i }`];
+            if(ingredient && ingredient.trim()) {
+                ingredients.push(`${ measure ? measure.trim() : '' } ${ ingredient.trim() }`);
+            }
+        }
+    }
+
     return(
-        <main className="flex flex-col items-center p-3">
-            <div className="w-full flex flex-row justify-between">
+        <main className="flex flex-col items-center py-8">
+            <div className="w-full flex flex-row justify-between max-w-[1136px] mb-[52px]">
                 <img src={ logo }/>
                 <Button
                     onClick={ handleBack }
@@ -40,12 +60,45 @@ const RecipeDetails = () => {
                     </div>
                 </Button>
             </div>
-            <section>
-                    <img src={ recipe.strMealThumb } className="w-[616px] h-[400px] object-cover rounded-lg" />
-                    <h1>{ recipe.strMeal }</h1>
-                    <h2>Coming soon...</h2>
+            <section className="w-[616px]">
+                    <img 
+                        src={ recipe.strMealThumb } 
+                        className="w-[616px] h-[400px] object-cover rounded-lg mb-8" 
+                    />
+                    <h1 className="text-[40px] text-[#E5E7EB] font-semibold mb-5">
+                        { recipe.strMeal }
+                    </h1>
+                    <div className="flex flex-row justify-start gap-3 mb-10">
+                        <InfoLabel>
+                            <span className="flex flex-row">
+                                category: <p>{ recipe.strCategory }</p>
+                            </span>
+                        </InfoLabel>
+                        <InfoLabel>
+                            <span className="flex flex-row">
+                                area: <p>{ recipe.strArea }</p>
+                            </span>
+                        </InfoLabel>
+                    </div>
+                    <div className="flex flex-col gap-4 mb-10">
+                        <div className="flex flex-row items-center gap-3 mb-4">
+                            <div className="w-4 h-8 rounded-xl bg-[#FEBD2E]"/>
+                            <p>Ingredients</p>
+                        </div>
+                        <ul className="flex flex-col items-start gap-3 list-disc">
+                            { ingredients.length !== 0 && ingredients.map((item) => <li key={ item }>{ item }</li>) }
+                        </ul>
+                    </div>
+                    <div className="flex flex-col gap-4">
+                        <div className="flex flex-row items-center gap-3 mb-4">
+                            <div className="w-4 h-8 rounded-xl bg-[#4E80EE]"/>
+                            <p>Instructions</p>
+                        </div>
+                        <p>
+                            { recipe.strInstructions }
+                        </p>
+                    </div>
                 </section>
-            
         </main>
     );
 };
