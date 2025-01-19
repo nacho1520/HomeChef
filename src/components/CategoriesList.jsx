bdf689ad4f72a8493d0d9e0dba633d816dedfe31
Ignacio Lopez <ignaciolopestela12@gmail.com>
2025-01-19 14:42:47 -0300

    add changes in categoryList component

--- src/components/CategoriesList.jsx
+++ src/components/CategoriesList.jsx
@@ -2,8 +2,15 @@ import { useState, useEffect } from "react";
 
 import { fetchData } from "../http.js";
 
-const CategoryButton = ({ children, ...props }) => {
-  return <button {...props}>{children}</button>;
+const CategoryButton = ({ category, selectedCategory, children, ...props }) => {
+  let cssClass = "border border-[#394150] w-[260px] h-14 rounded-xl";
+  if (+category.idCategory === selectedCategory.id) {
+    cssClass += " bg-[#FEBD2E] text-[#0E1325]";
+  } else {
+    cssClass += " bg-[#0E1325]";
+  }
+
+  return <button className={ cssClass } {...props}>{children}</button>;
 };
 
 const CategoriesList = ({ selectedCategory, onSelect }) => {
@@ -12,7 +19,7 @@ const CategoriesList = ({ selectedCategory, onSelect }) => {
   useEffect(() => {
     async function fetchCategories() {
       try {
-        const categoriesData = await fetchData('categories');
+        const categoriesData = await fetchData("categories");
 
         setCategories(categoriesData.categories);
       } catch (error) {
@@ -24,28 +31,28 @@ const CategoriesList = ({ selectedCategory, onSelect }) => {
   }, []);
 
   return (
-    <aside>
-      <h2>Categories</h2>
+    <aside className="font-body">
+      <h2 className="font-title text-2xl font-semibold">Categories</h2>
       <ul className="pt-8 flex flex-col gap-3">
         {categories.length !== 0 &&
           categories.map((category) => {
-            let cssClass = "border border-[#394150] w-[260px] h-14 rounded-xl";
-            if (+category.idCategory === selectedCategory.id) {
-              cssClass += " bg-[#FEBD2E] text-[#0E1325]";
-            } else {
-              cssClass += " bg-[#0E1325]";
-            }
-
+            
             return (
               <li key={category.idCategory}>
                 <CategoryButton
-                  className={cssClass}
+                  category={ category }
+                  selectedCategory={ selectedCategory }
                   onClick={() => onSelect(category)}
                 >
-                    <div className="flex flex-row items-center gap-2">
-                        <img src={ category.strCategoryThumb } className="w-11 h-full" />
-                        <p>{ category.strCategory}</p>
-                    </div>  
+                  <div className="flex flex-row items-center gap-2">
+                    <img
+                      src={category.strCategoryThumb}
+                      className="w-11 h-full"
+                    />
+                    <p className="text-xs font-semibold">
+                      {category.strCategory}
+                    </p>
+                  </div>
                 </CategoryButton>
               </li>
             );
